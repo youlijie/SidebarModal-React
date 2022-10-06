286abfc89ea975aeb5998b1cfc7973374f5ea686
youlijie <63795226+youlijie@users.noreply.github.com>
2022-10-06 12:34:16 +0200

    12 project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Modal from './Modal'
+import Sidebar from './Sidebar'
+import Home from './Home'
+function App() {
+  return (
+    <>
+      <Home/>
+      <Modal/>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
